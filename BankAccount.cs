8befd79c64a7601f7fe9be67655b9d8184d33f6a
Besart Elezi <besart.elezi@hotmail.com>
2022-09-13 15:53:12 +0200

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System.Dynamic;
+
+namespace a;
+
+public class BankAccount
+{
+    private int id;
+
+    public int myId
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public BankAccount(int myId)
+    {
+        this.myId = myId;
+    }
+
+    public string tellID
+    {
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using a;
+
+Console.WriteLine("Hello, World!");
+
+BankAccount bankAccount = new BankAccount(1);
