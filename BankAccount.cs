bf58512e818da8db79cd9a40246a0c84d8ed5f2b
Besart Elezi <besart.elezi@hotmail.com>
2022-09-13 16:51:51 +0200

    updated BankAccount class

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,25 +1,52 @@
-﻿using System.Dynamic;
-
-namespace a;
+﻿namespace a;
 
 public class BankAccount
 {
-    private int id;
+    //properties
+    private string clientName;
+    private int balance;
+    private string type;
 
-    public int myId
+    //constructor
+    public BankAccount(string clientName, int balance, string type)
     {
-        get { return id; }
-        set { id = value; }
+        this.clientName = clientName;
+        this.balance = balance;
+        this.type = type;
     }
 
-    public BankAccount(int myId)
+    //getters and setters
+    public string _clientName
     {
-        this.myId = myId;
+        get { return clientName; }
+        set { clientName = value; }
     }
 
-    public string tellID
+    public int _balance
     {
-        
+        get { return balance; }
+        set { balance = value; }
     }
 
+    public string _type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    //methods
+    public string CheckBalance
+    {
+        get { return "your current balance is " + balance; }
+    }
+
+    public int Withdraw
+    {
+        set { balance -= value; }
+    }
+
+    public int Deposit
+    {
+        set { balance += value; }
+    }
 }
