ad17ae86a86f57f64afbe2b0fda1f392f2d60289
Besart Elezi <besart.elezi@hotmail.com>
2022-09-14 15:48:07 +0200

    added Client class

---
+++ Client.cs
@@ -0,0 +1,36 @@
+﻿namespace a;
+
+public class Client
+{
+    //properties
+    private int id;
+    private string name;
+    private string dateJoined;
+    
+    //constructor
+    public Client(int id, string name, string dateJoined)
+    {
+        this.id = id;
+        this.name = name;
+        this.dateJoined = dateJoined;
+    }
+    
+    //getters and setters
+    public int Id
+    {
+        get => id;
+        set => id = value;
+    }
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string DateJoined
+    {
+        get => dateJoined;
+        set => dateJoined = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
