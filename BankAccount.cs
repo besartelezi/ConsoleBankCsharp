3937b3e1bfab6fd8569ce14ada4ea9a3fa9c34fc
Besart Elezi <besart.elezi@hotmail.com>
2022-09-15 16:27:14 +0200

    refactored some code + added CheckProfile method
    replaced if statements by switch statement

--- BankAccount.cs
+++ BankAccount.cs
@@ -3,23 +3,21 @@
 public class BankAccount
 {
     //properties
-    private string _clientName;
+    private Client _client;
     private int _balance;
     private string _type;
 
-    //constructor
-    public BankAccount(string clientName, int balance, string type)
+    public BankAccount(Client client, int balance, string type)
     {
-        _clientName = clientName;
+        _client = client;
         _balance = balance;
         _type = type;
     }
 
-    //getters and setters
-    public string ClientName
+    public Client Client
     {
-        get => _clientName;
-        set => _clientName = value ?? throw new ArgumentNullException(nameof(value));
+        get => _client;
+        set => _client = value ?? throw new ArgumentNullException(nameof(value));
     }
 
     public int Balance
@@ -37,11 +35,16 @@ public class BankAccount
     //methods
     public void Withdraw()
     {
-        //make it impossible to withdraw amount higher than current balance
         int withdrawAmount;
         Console.WriteLine("How much euros would you like to withdraw?");
-
         withdrawAmount = Convert.ToInt32(Console.ReadLine());
+        
+        while (withdrawAmount > _balance)
+        {
+            Console.WriteLine("I'm sorry, but you can't withdraw an amount that's higher than your current balance. \n Please select a new amount you'd like to withdraw.");
+            withdrawAmount = Convert.ToInt32(Console.ReadLine());
+        }
+        
         _balance -= withdrawAmount;
 
         Console.WriteLine($"You have successfully withdrawn: {withdrawAmount} euros.");
@@ -55,6 +58,12 @@ public class BankAccount
         Console.WriteLine("How much euros would you like to deposit?");
 
         depositAmount = Convert.ToInt32(Console.ReadLine());
+        while (depositAmount <=0)
+        {
+            Console.WriteLine("I'm sorry, but you can't deposit an amount that's lower than or equal to zero. \n Please select a new amount you'd like to deposit.");
+            depositAmount = Convert.ToInt32(Console.ReadLine());
+        }
+        
         _balance += depositAmount;
 
         Console.WriteLine($"You have successfully deposited: {depositAmount} euros.");
@@ -67,40 +76,45 @@ public class BankAccount
         HandleBanking();
     }
 
+    public void CheckProfile()
+    {
+        Console.WriteLine($"Your name is: {Client.Name} \nYou joined on {Client.DateJoined} and your ID number is {Client.Id}");
+        HandleBanking();
+    }
+
     public void CloseBanking()
     {
-        Console.WriteLine($"Have a good day {_clientName}");
+        Console.WriteLine($"Have a good day {Client.Name}");
     }
 
     public void HandleBanking()
     {
-        Console.WriteLine($"What would you like to do now {_clientName}? \n a. Check your balance \n b. Deposit money \n c. Withdraw money \n d. Close the banking console");
+        Console.WriteLine($"What would you like to do now {_client.Name}? \n a. Check your balance \n b. Deposit money \n c. Withdraw money \n d. Check your profile \n e. Close the banking console");
         string handleBankingAnswer;
         handleBankingAnswer = Console.ReadLine();
-        while (!(handleBankingAnswer == "a" || handleBankingAnswer == "b"|| handleBankingAnswer == "c" || handleBankingAnswer == "d"))
+        while (!(handleBankingAnswer == "a" || handleBankingAnswer == "b"|| handleBankingAnswer == "c" || handleBankingAnswer == "d" || handleBankingAnswer == "e"))
         {
-            Console.WriteLine("Please pick an appropriate answer. \n a. Check your balance \n b. Deposit money \n c. Withdraw money \n d. Close the banking console");
+            Console.WriteLine("Please pick an appropriate answer. \n a. Check your balance \n b. Deposit money \n c. Withdraw money \n d. Check your profile \n e. Close the banking console");
             handleBankingAnswer = Console.ReadLine();
         }
 
-        if (handleBankingAnswer == "a")
+        switch (handleBankingAnswer)
         {
-            CheckBalance();
-        }
-
-        if (handleBankingAnswer == "b")
-        {
-            Deposit();
-        }
-
-        if (handleBankingAnswer == "c")
-        {
-            Withdraw();
-        }
-
-        if (handleBankingAnswer == "d")
-        {
-            CloseBanking();
+            case "a":
+                CheckBalance();
+                break;
+            case "b":
+                Deposit();
+                break;
+            case "c":
+                Withdraw();
+                break;
+            case "d":
+                CheckProfile();
+                break;
+            case "e":
+                CloseBanking();
+                break;
         }
     }
 }
