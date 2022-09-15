467730405452f9b957d3db237b768d65aad0d094
Besart Elezi <besart.elezi@hotmail.com>
2022-09-15 12:16:44 +0200

    updated HandleBanking method

--- BankAccount.cs
+++ BankAccount.cs
@@ -39,31 +39,39 @@ public class BankAccount
     //methods
     public void Withdraw()
     {
+        //make it impossible to withdraw amount higher than current balance
         int withdrawAmount;
-        CheckBalance();
         Console.WriteLine("How much euros would you like to withdraw?");
 
         withdrawAmount = Convert.ToInt32(Console.ReadLine());
         balance -= withdrawAmount;
 
         Console.WriteLine($"You have successfully withdrawn: {withdrawAmount} euros.");
+        handleBanking();
     }
     
     public void Deposit()
     {
+        //make it impossible to deposit negative numbers
         int depositAmount;
-        CheckBalance();
         Console.WriteLine("How much euros would you like to deposit?");
 
         depositAmount = Convert.ToInt32(Console.ReadLine());
         balance += depositAmount;
 
         Console.WriteLine($"You have successfully deposited: {depositAmount} euros.");
+        handleBanking();
     }
     
     public void CheckBalance()
     {
         Console.WriteLine($"Your current balance is: {balance}");
+        handleBanking();
+    }
+
+    public void closeBanking()
+    {
+        Console.WriteLine($"Have a good day {clientName}");
     }
 
     public void handleBanking()
@@ -73,7 +81,28 @@ public class BankAccount
         handleBankingAnswer = Console.ReadLine();
         while (!(handleBankingAnswer == "a" || handleBankingAnswer == "b"|| handleBankingAnswer == "c" || handleBankingAnswer == "d"))
         {
-            Console.WriteLine("Please pick an appropriate answer. ");
+            Console.WriteLine("Please pick an appropriate answer. \n a. Check your balance \n b. Deposit money \n c. Withdraw money \n d. Close the banking console");
+            handleBankingAnswer = Console.ReadLine();
+        }
+
+        if (handleBankingAnswer == "a")
+        {
+            CheckBalance();
+        }
+
+        if (handleBankingAnswer == "b")
+        {
+            Deposit();
+        }
+
+        if (handleBankingAnswer == "c")
+        {
+            Withdraw();
+        }
+
+        if (handleBankingAnswer == "d")
+        {
+            closeBanking();
         }
     }
 }
